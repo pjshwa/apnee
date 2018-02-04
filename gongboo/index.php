229bdfae95389befd33bb8fde3fd1de11b209ba4
pjshwa <pjshwa@gmail.com>
2018-02-04 16:27:15 +0900

    EDIT - '공부' 페이지를 카테고리별로 볼 수 있도록 업데이트

--- gongboo/index.php
+++ gongboo/index.php
@@ -1,10 +1,10 @@
 <?php
 require('db.php');
-$articles = $db->titlesOfMemo();
+$category_set = $db->limitAllMemosByCategory(3);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>	
+<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,19 +21,25 @@ $articles = $db->titlesOfMemo();
 	<!-- Body -->
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<ul>
+				<a href="list.php"><h4>공부 페이지</h4></a>
 				<?php
-				if (count($articles) === 0){
-					echo '<article><h4>글이 없어요</h4></article>';
+				if (count($category_set) === 0){
+					echo '<div class="col-md-12"><ul><article><h4>글이 없어요</h4></article></ul></div>';
 				}
 				else {
-					foreach($articles as $article){
-						echo '<li><a href=./memo.php?id='.$article['id'].'>'.$article['title'].'</a> ('.date('Y년 n월 j일', strtotime($article['date'])).')</li>';
+					foreach($category_set as $category_data){
+						echo '<div class="col-md-4">';
+						echo '<h5>'.$category_data['title'].'</h5>';
+						echo '<ul>';
+						foreach($category_data['articles'] as $article){
+							echo '<li><a href=./memo.php?id='.$article['id'].'>'.$article['title'].'</a></li>';
+						}
+						echo '</ul>';
+						echo '<a href="list.php?category_id='.$category_data['id'].'"><h6>'.$category_data['title'].'에 대한 글 모두 보기</h6></a>';
+						echo '</div>';
 					}
-				}?>
-				</ul>
-			</div>
+				}
+				?>
 		</div>
 	</div>
 </body>
---
+++ gongboo/list.php
@@ -0,0 +1,48 @@
+<?php
+require('db.php');
+if (isSet($_GET['category_id'])){
+  $category_id = (int)$_GET['category_id'];
+}
+else {
+  $category_id = 0;
+}
+$result = $db->titlesOfMemo($category_id);
+$category_title = $result[0];
+$articles = $result[1];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>공부</title>
+	<link href="../static/css/bootstrap.min.css" rel="stylesheet" />
+	<script src="../static/js/jquery.js"></script>
+	<script src="../static/js/bootstrap.min.js"></script>
+</head>
+<body style="margin-top: 75px;">
+	<!-- Body -->
+	<div class="container">
+		<div class="row">
+      <?php echo '<h4>'.$category_title.'에 대한 글 ('.count($articles).')</h4>'; ?>
+			<div class="col-md-12">
+				<ul>
+				<?php
+				if (count($articles) === 0){
+					echo '<article><h4>글이 하나도 없어요</h4></article>';
+				}
+				else {
+					foreach($articles as $article){
+						echo '<li><a href=./memo.php?id='.$article['id'].'>'.$article['title'].'</a> ('.date('Y년 n월 j일', strtotime($article['date'])).')</li>';
+					}
+				}?>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
