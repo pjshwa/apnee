e18109ddcfbeee689c6cac9ac5cc285029435d8c
jinsik park <adelaww@naver.com>
2020-03-29 00:17:42 +0900

    ADD - static file 을 git 에서 관리한다

---
+++ static/js/cow.js
@@ -0,0 +1,133 @@
+var score = 0;
+var counter = 15;
+var score_span = document.getElementById("score");
+var count_span = document.getElementById("count");
+var sound_check = document.getElementsByName("sound")[0];
+var pre_cows = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        pre_cows[i] = new Image();
+        pre_cows[i].src = preload.arguments[i];
+}}
+
+preload(
+    "./cows/n_1.png",
+    "./cows/n_2.png",
+    "./cows/n_3.png",
+    "./cows/n_4.png",
+    "./cows/n_5.png",
+    "./cows/n_6.png",
+    "./cows/y_1.png",
+    "./cows/y_2.png",
+    "./cows/y_3.png",
+    "./cows/y_4.png",
+    "./cows/y_5.png",
+    "./cows/y_6.png"
+)
+
+$("#startClock").click( function(){
+    document.getElementById("intro").style.visibility = "hidden";
+    document.getElementById("startClock").style.visibility = "hidden";
+    document.getElementById("front_row").style.height = "0px";
+    document.getElementById("front_image").style.height = "0px";
+    document.getElementById("scoreboard").style.height = "0px";
+    document.getElementById("scoreboard").style.visibility = "hidden";
+    document.getElementById("sound_box").style.visibility = "hidden";
+    document.getElementById("ig").style.visibility = "visible";
+    refreshCows();
+   setInterval(function() {
+     counter--;
+      if (counter >= 0) {
+         count_span.innerHTML = counter;
+      }
+      if (counter === 0) {
+         gameOver();
+         clearInterval(counter);
+       }
+       if (counter > 15 || score > 30) {
+         cheat();
+         clearInterval(counter);
+       }
+     }, 1000);
+});
+
+const yes_prompts = ["얼룩소를 고르세요", "얼룩소를 찾으세요", "얼룩소가 아닌 소를 고르지 마세요", "얼룩 무늬가 있는 소를 고르세요"];
+const no_prompts = ["얼룩소를 고르지 마세요", "얼룩소가 아닌 소를 고르세요", "얼룩소를 고르지 마세요", "얼룩소가 아닌 소를 고르지 말지 마세요"];
+var prompt = 0;
+var prompt_text = "";
+var answer = 0;
+var prompt_span = document.getElementById('prompt_span');
+var cows = [];
+
+for(var i = 0; i < 4; i++){
+  cows.push(document.getElementById('cow_'+(i+1)));
+}
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min); // rand number generator
+}
+
+function refreshCows(){
+  prompt = randomIntFromInterval(0, 3);
+  var yes_or_no = randomIntFromInterval(0, 1);
+  if (yes_or_no === 0) {
+    prompt_text = no_prompts[prompt];
+  }
+  else {prompt_text = yes_prompts[prompt];}
+  prompt_span.innerHTML = prompt_text;
+  answer = randomIntFromInterval(0, 3);
+  for(var i = 0; i < 4; i++){
+    if (i === answer) {
+      cows[i].setAttribute('onclick','movingOn()');
+      if (yes_or_no === 0) {
+        cows[i].src = './cows/n_'+randomIntFromInterval(1, 6)+'.png';
+      }
+      else {cows[i].src = './cows/y_'+randomIntFromInterval(1, 6)+'.png';}
+    }
+    else { cows[i].setAttribute('onclick','gameOver()');
+      if (yes_or_no === 0) {
+          cows[i].src = './cows/y_'+randomIntFromInterval(1, 6)+'.png';
+        }
+        else {cows[i].src = './cows/n_'+randomIntFromInterval(1, 6)+'.png';}
+      }
+  }
+}
+
+var cow_01 = new Audio('./m/moo1.ogg');
+var cow_02 = new Audio('./m/moo2.ogg');
+var cow_03 = new Audio('./m/moo3.ogg');
+var cow_04 = new Audio('./m/moo4.ogg');
+
+
+
+function playSound(){
+    var sound_pick = randomIntFromInterval(1, 4);
+    if (sound_pick === 1) cow_01.play();
+    else if (sound_pick === 2) cow_02.play();
+    else if (sound_pick === 3) cow_03.play();
+    else if (sound_pick === 4) cow_04.play();
+}
+
+function movingOn(){
+    score++;
+    score_span.innerHTML = score;
+    refreshCows();
+    if (sound_check.checked){
+        playSound();
+    }
+}
+
+function gameOver() {
+    window.alert("게임 끝");
+    window.location.reload();
+    document.getElementById("sc").value = document.getElementById("score").innerHTML;
+    document.forms["score"].submit();
+}
+
+
+function cheat() {
+    window.alert("사기꾼");
+    var loc = window.location;window.location = loc.protocol + '//' + loc.host + loc.pathname + loc.search;
+    // force GET
+}
