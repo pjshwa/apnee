f40c2addbdedf46de675aaf1ff4a35c8f604aa0c
pjshwa <pjshwa@gmail.com>
2024-03-02 14:24:13 +0900

    EDIT redirect to home if url is null

--- leet.php
+++ leet.php
@@ -4,6 +4,10 @@
   $now = gmdate('Y-m-d');
   $url = $db->getDailyLeetCode($now);
 
+  if ($url == null) {
+    $url = "https://www.leetcode.com";
+  }
+
   // Redirect to the daily leetcode problem
   header("Location: $url");
 ?>
