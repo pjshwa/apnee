86df765bb51b5823f875e918a1f3fbe3ee657b37
pjshwa <pjshwa@gmail.com>
2026-09-13 17:31:21 +0900

    fix: validate page numbers in judge pages

--- foretooth/check.php
+++ foretooth/check.php
@@ -2,6 +2,9 @@
 require('db.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 $item = $db->getanswer($_POST['q_key']);
+$page = filter_var($_POST['q_page'] ?? null, FILTER_VALIDATE_INT, [
+    'options' => ['min_range' => 1]
+]) ?: 1;
 
 if ($item === $_POST['ans']){
 	echo "<script>alert('맞았다');";
@@ -11,7 +14,7 @@ else {
 	echo "<script>alert('틀렸다');";
 	$db->updateTries($_POST['q_key'], False);
 }
-echo "location.href='catalog.php?page=".$_POST['q_page']."';</script>";
+echo "location.href='catalog.php?page=".$page."';</script>";
 }
 else {
 	header("Location: catalog.php");
